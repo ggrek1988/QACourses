b9b5ac47e443be0e668794af89e1c5c524e354dc
Grzegorz Gajownik <93194238+ggrek1988@users.noreply.github.com>
2022-03-03 12:54:01 +0100

    Add files via upload

---
+++ test_findsticker_task08.py
@@ -0,0 +1,36 @@
+import pytest
+from selenium import  webdriver
+from selenium.webdriver.chrome.service import Service
+
+
+@pytest.fixture(scope="session", autouse=True)
+def driver(request):
+    ser = Service("C:\\driver_firefox_selenium\\geckodriver.exe")
+    op = webdriver.FirefoxOptions()
+    wd = webdriver.Firefox(service=ser, options=op)
+    request.addfinalizer(wd.quit)
+    return wd
+
+def testfindstikers(driver):
+
+    driver.get("http://localhost:82/litecart/")
+    elementduck = len(driver.find_elements_by_xpath("//div[@id='content']//article[@class='product']"))
+    salestikers_list = len(driver.find_elements_by_xpath("//article[@class='product']//div[@title='On Sale']"))
+    newstiker_list = len(driver.find_elements_by_xpath("//article[@class='product']//div[@title='New']"))
+
+    #checking if the number of stickers is equal to the number of articles
+    assert elementduck == salestikers_list + newstiker_list
+
+    for elduck in range(elementduck):
+
+        try:
+            driver.find_elements_by_xpath("//article[@class='product']//div[starts-with(@class, 'sticker')]")[elduck]
+        except:
+            raise Exception("The article has more than one sticker")
+
+
+
+
+
+
+
